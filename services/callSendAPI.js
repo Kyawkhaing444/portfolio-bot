10a500f4122eb3ae31489f014ef3f6f4f79b6413
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-01-19 12:32:39 +0630

    Structured folder

---
+++ services/callSendAPI.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+module.exports = function callSendAPI(sender_psid, response) { // Sends response messages via the Send API
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v9.0/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+};
