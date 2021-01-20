bbcf6d4bb85af86339a673770700cd3560e80d48
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-01-20 11:52:22 +0630

    quick reply

--- controllers/handlePostback.js
+++ controllers/handlePostback.js
@@ -1,7 +1,7 @@
 const callSendAPI = require("./callSendAPI");
 const fetch = require("node-fetch");
-const persistentMenu = require("../components/persistentMenu");
-const quickReply = require("../components/quickReply");
+// const persistentMenu = require("../components/persistentMenu");
+// const quickReply = require("../components/quickReply");
 const workExperience = require("../components/workExperience");
 const education = require("../components/education");
 const projects = require("../components/projects");
@@ -43,4 +43,72 @@ module.exports = function handlePostback(sender_psid, received_postback) {
     response = education();
     callSendAPI(sender_psid, response);
   }
+};
+
+const quickReply = () => {
+  return {
+    text: `Hello! Welcome to Kyaw Khaing's world! You can ask whatever you want about Kyaw Khaing`,
+    quick_replies: [
+      {
+        content_type: "text",
+        title: "Work",
+        payload: "work",
+      },
+      {
+        content_type: "text",
+        title: "Projects",
+        payload: "projects",
+      },
+      {
+        content_type: "text",
+        title: "Skills",
+        payload: "skills",
+      },
+      {
+        content_type: "text",
+        title: "Education",
+        payload: "Education",
+      },
+    ],
+  };
+};
+
+const persistentMenu = () => {
+  return {
+    psid: sender_psid,
+    persistent_menu: [
+      {
+        locale: "default",
+        composer_input_disabled: false,
+        call_to_actions: [
+          {
+            type: "postback",
+            title: "Work Experience",
+            payload: "work",
+          },
+          {
+            type: "postback",
+            title: "Projects",
+            payload: "projects",
+          },
+          {
+            type: "postback",
+            title: "Technical Skills",
+            payload: "skills",
+          },
+          {
+            type: "postback",
+            title: "Education",
+            payload: "education",
+          },
+          {
+            type: "web_url",
+            title: "Download Resume",
+            url: "http://bit.ly/kkkresume",
+            webview_height_ratio: "full",
+          },
+        ],
+      },
+    ],
+  };
 };
