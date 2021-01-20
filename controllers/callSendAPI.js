ed34417c773eb6d7b96a84dc6cda84d72cd5ecef
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-01-20 14:44:20 +0630

    Introtext fixed

--- controllers/callSendAPI.js
+++ controllers/callSendAPI.js
@@ -69,6 +69,7 @@ module.exports = function callSendAPI(sender_psid, response) {
             id: sender_psid,
           },
           message: {
+            text: ``,
             quick_replies: [
               {
                 content_type: "text",
