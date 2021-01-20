b47d9d283f486a995e05c655d9f6adf4603d5c89
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-01-20 15:45:21 +0630

    Introtext fixed

--- controllers/callSendAPI.js
+++ controllers/callSendAPI.js
@@ -5,48 +5,48 @@ module.exports = function callSendAPI(sender_psid, response) {
     "access_token=" + encodeURIComponent(process.env.PAGE_ACCESS_TOKEN);
 
   if (response.persistent_menu !== undefined) {
-    return fetch(
-      "https://graph.facebook.com/v9.0/me/custom_user_settings?" + qs,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(response),
-      }
-    ).then(() => {
-      return fetch("https://graph.facebook.com/v9.0/me/messages?" + qs, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          recipient: {
-            id: sender_psid,
-          },
-          message: {
-            text: `Hello! Welcome to Kyaw Khaing's world! You can ask whatever you want about Kyaw Khaing`,
-            quick_replies: [
-              {
-                content_type: "text",
-                title: "Work",
-                payload: "work",
-              },
-              {
-                content_type: "text",
-                title: "Projects",
-                payload: "projects",
-              },
-              {
-                content_type: "text",
-                title: "Skills",
-                payload: "skills",
-              },
-              {
-                content_type: "text",
-                title: "Education",
-                payload: "Education",
-              },
-            ],
-          },
-        }),
-      });
+    return fetch("https://graph.facebook.com/v9.0/me/messages?" + qs, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        recipient: {
+          id: sender_psid,
+        },
+        message: {
+          text: `Hello! Welcome to Kyaw Khaing's world! You can ask whatever you want about Kyaw Khaing`,
+          quick_replies: [
+            {
+              content_type: "text",
+              title: "Work",
+              payload: "work",
+            },
+            {
+              content_type: "text",
+              title: "Projects",
+              payload: "projects",
+            },
+            {
+              content_type: "text",
+              title: "Skills",
+              payload: "skills",
+            },
+            {
+              content_type: "text",
+              title: "Education",
+              payload: "Education",
+            },
+          ],
+        },
+      }),
+    }).then(() => {
+      return fetch(
+        "https://graph.facebook.com/v9.0/me/custom_user_settings?" + qs,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(response),
+        }
+      );
     });
   } else {
     let request_body = {
