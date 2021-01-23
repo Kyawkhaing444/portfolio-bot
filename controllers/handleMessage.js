d4fc176f7eb2d85319eec86378e69e09693f7060
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-01-23 15:08:36 +0630

    final test

--- controllers/handleMessage.js
+++ controllers/handleMessage.js
@@ -7,19 +7,18 @@ const callSendAPI = require("./callSendAPI");
 module.exports = function handleMessage(sender_psid, received_message) {
   let response;
   // Check if the message contains text
-  if (received_message === "Work") {
+  if (received_message.text === "Work") {
     response = workExperience();
-  } else if (received_message === "Projects") {
+  } else if (received_message.text === "Projects") {
     response = projects();
-  } else if (received_message === "Education") {
+  } else if (received_message.text === "Education") {
     response = education();
-  } else if (received_message === "Skills") {
+  } else if (received_message.text == "Skills") {
     response = skills();
   } else {
     response = {
       text: "Please use the persistent menu and quick reply",
     };
   }
-  console.log(response);
   callSendAPI(sender_psid, response);
 };
