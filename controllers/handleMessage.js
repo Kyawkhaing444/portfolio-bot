d0a2d5f5c8489479a624a871452657a900a59dd3
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-01-23 14:21:33 +0630

    Merge branch 'main' of https://github.com/Kyawkhaing444/portfolio-bot into main

--- controllers/handleMessage.js
+++ controllers/handleMessage.js
@@ -7,50 +7,19 @@ const callSendAPI = require("./callSendAPI");
 module.exports = function handleMessage(sender_psid, received_message) {
   let response;
   // Check if the message contains text
-  if (received_message.text) {
-    if(received_message.text === "Work"){
-       response = workExperience();
-    }else if(received_message.text === "Projects"){
-      response = projects();
-    }else if(received_message.text === "Education"){
-      response = education();
-    }else if(received_message.text === "Skills"){
-      response = skills();
-    }else{
-      response = {
-        "text": "Please use the persistent menu and quick reply"
-      }
-    }
-    callSendAPI(sender_psid, response);
-  } else if (received_message.attachments) {
-    // Gets the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
+  if (received_message.text === "Work") {
+    response = workExperience();
+  } else if (received_message.text === "Projects") {
+    response = projects();
+  } else if (received_message.text === "Education") {
+    response = education();
+  } else if (received_message.text === "Skills") {
+    response = skills();
+  } else {
     response = {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [
-            {
-              title: "Is this the right picture?",
-              subtitle: "Tap a button to answer.",
-              image_url: attachment_url,
-              buttons: [
-                {
-                  type: "postback",
-                  title: "Yes!",
-                  payload: "yes",
-                },
-                {
-                  type: "postback",
-                  title: "No!",
-                  payload: "no",
-                },
-              ],
-            },
-          ],
-        },
-      },
+      text: "Please use the persistent menu and quick reply",
     };
   }
+  console.log(response);
+  callSendAPI(sender_psid, response);
 };
