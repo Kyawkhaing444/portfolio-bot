99640c5dbd67d39484c8c2ee6b6de2568b759eab
Kyaw Kyaw Khaing <kyawkyawkhaing@ucsm.edu.mm>
2021-01-23 14:27:04 +0630

    Revert "Introtext fixed"
    This reverts commit c498f90a68df25bf96421d247563b3096cb8b5f9.

--- controllers/callSendAPI.js
+++ controllers/callSendAPI.js
@@ -1,17 +1,14 @@
-const { default: axios } = require("axios");
 const fetch = require("node-fetch");
 
-module.exports = async function callSendAPI(sender_psid, response) {
+module.exports = function callSendAPI(sender_psid, response) {
   const qs =
     "access_token=" + encodeURIComponent(process.env.PAGE_ACCESS_TOKEN);
-  const URL = "https://graph.facebook.com/v9.0/me/messages?" + qs;
-  const persistentURL =
-    "https://graph.facebook.com/v9.0/me/custom_user_settings?" + qs;
 
   if (response.persistent_menu !== undefined) {
-    const responseQuickReply = await axios.post(
-      URL,
-      JSON.stringify({
+    return fetch("https://graph.facebook.com/v9.0/me/messages?" + qs, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
         recipient: {
           id: sender_psid,
         },
@@ -40,25 +37,17 @@ module.exports = async function callSendAPI(sender_psid, response) {
             },
           ],
         },
-      })
-    );
-    const { data } = responseQuickReply;
-    if (data.error) {
-      throw new Error(
-        `Send API Request Failed ## Code (${data.error.code}) ##`
+      }),
+    }).then(() => {
+      return fetch(
+        "https://graph.facebook.com/v9.0/me/custom_user_settings?" + qs,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(response),
+        }
       );
-    }
-    const responsePersistent = await axios.post(
-      persistentURL,
-      JSON.stringify(response)
-    );
-    const { dataper } = responsePersistent;
-    if (dataper.error) {
-      throw new Error(
-        `Send API Request Failed ## Code (${dataper.error.code}) ##`
-      );
-    }
-    return;
+    });
   } else {
     let request_body = {
       recipient: {
@@ -67,64 +56,45 @@ module.exports = async function callSendAPI(sender_psid, response) {
       message: response,
     };
 
-    try {
-      const responseNormalText = await axios.post(
-        URL,
-        JSON.stringify("11")
-      );
-      console.log("data", responseNormalText);
-      const { data } = responseNormalText;
-      console.log("data", data.error);
-    } catch (e) {
-      throw new Error(`Send API Request Failed ## Code (${e}) ##`);
-    }
-    if (data.error) {
-      throw new Error(
-        `Send API Request Failed ## Code (${data.error.code}) ##`
-      );
-    }
-
-    const responseQuickReply = await axios.post(
-      URL,
-      JSON.stringify({
-        recipient: {
-          id: sender_psid,
-        },
-        message: {
-          text: `What else do you want to ask?`,
-          quick_replies: [
-            {
-              content_type: "text",
-              title: "Work",
-              payload: "work",
-            },
-            {
-              content_type: "text",
-              title: "Projects",
-              payload: "projects",
-            },
-            {
-              content_type: "text",
-              title: "Skills",
-              payload: "skills",
-            },
-            {
-              content_type: "text",
-              title: "Education",
-              payload: "Education",
-            },
-          ],
-        },
-      })
-    );
-
-    const { dataQuick } = responseQuickReply;
-
-    if (dataQuick.error) {
-      throw new Error(
-        `Send API Request Failed ## Code (${dataQuick.error.code}) ##`
-      );
-    }
-    return;
+    return fetch("https://graph.facebook.com/v9.0/me/messages?" + qs, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(request_body),
+    }).then(() => {
+      return fetch("https://graph.facebook.com/v9.0/me/messages?" + qs, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          recipient: {
+            id: sender_psid,
+          },
+          message: {
+            text: `What else do you want to ask?`,
+            quick_replies: [
+              {
+                content_type: "text",
+                title: "Work",
+                payload: "work",
+              },
+              {
+                content_type: "text",
+                title: "Projects",
+                payload: "projects",
+              },
+              {
+                content_type: "text",
+                title: "Skills",
+                payload: "skills",
+              },
+              {
+                content_type: "text",
+                title: "Education",
+                payload: "Education",
+              },
+            ],
+          },
+        }),
+      });
+    });
   }
 };
